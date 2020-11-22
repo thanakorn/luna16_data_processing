c2d3d5ec3e98277b5cc7fd7ad8f070eea6591f93
thanakorn <thanakorn.panyapiang@outlook.com>
2020-11-22 18:39:02 +0700

    Script for making training set

---
+++ make_train_set.sh
@@ -0,0 +1,24 @@
+sudo apt-get install p7zip-full
+
+mkdir data
+
+echo "Downloading Annotations"
+wget https://zenodo.org/record/3723295/files/annotations.csv -P ./data/
+
+for i in 0 1 2 3 4 5 6
+do
+  echo 'Downloading subset$i'
+  wget https://zenodo.org/record/3723295/files/subset$i.zip -P ./data/
+  7z x ./data/subset$i.zip
+  mv subset$i ./data/
+  rm subset$i.zip
+done
+
+mkdir /content/train_raw
+cp ./data/annotations.csv /content/train_raw/
+for i in 0 1 2 3 4 5 6
+do
+  rsync -vau ./data/subset$i/ /content/train_raw/
+done
+
+python to_coco_format.py --data=/content/train_raw --output=/content/LUNA --num_processes=4
